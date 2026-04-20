b4124eedef223945259e3cf6982757f687c92906
Christoffer Hald Christensen <chrishald@proton.me>
2026-04-20 14:54:23 +0200

    feat(nix): Added nix flake support

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A Relationship-based Access Control service for 2nd semester university project at AAU.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs_latest
+      ];
+    };
+  };
+}
