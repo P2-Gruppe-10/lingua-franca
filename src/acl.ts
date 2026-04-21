09c3c7f5e8f4c1f862f4aef961ba576ee5a12c12
vicco1337 <hustedacount@gmail.com>
2026-04-21 13:51:43 +0200

    feat(acl): Added basic types

---
+++ src/acl.ts
@@ -0,0 +1,57 @@
+type Id = number;
+type Type = string;
+/**
+ * A thing which a subject can have a relation to.
+ * Consist of a type and a unique identifier.
+ */
+class Obj {
+
+    type: Type;
+    identifier: Id;
+
+    constructor(type: Type, identifier: Id) {
+        this.type = type;
+        this.identifier = identifier;
+    }
+
+
+}
+
+
+type Relation = string;
+/**
+ * Represents a set of users that all share an relation to an object
+ */
+class UserSet {
+
+    object: Obj;
+    relation: Relation;
+
+    constructor(object: Obj, relation: Relation) {
+        this.object = object;
+        this.relation = relation;
+    }
+
+}
+
+type UserId = number;
+type Subject = UserId | UserSet;
+/**
+ * The description of a relation between subjects and an object.
+ */
+class ACL {
+
+    object: Obj;
+    relation: Relation;
+    subject: Subject;
+
+    constructor(object: Obj, relation: Relation, subject: Subject) {
+
+        this.object = object;
+        this.relation = relation;
+        this.subject = subject;
+
+    }
+
+}
+
