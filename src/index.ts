af0c1712f7a86b8742b7bf8761b42fe84a894484
Christoffer Hald Christensen <chrishald@proton.me>
2026-04-30 10:00:36 +0200

    feat(api): post relations endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express from "express";
+import express, { type Request } from "express";
 import bodyParser from "body-parser";
-import { Obj, UserSet } from "./acl.ts";
+import { Obj, UserSet, type Subject } from "./acl.ts";
 import { deserializeConfig } from "./serialize.ts";
 
 const app = express();
@@ -36,6 +36,74 @@ app.get("/authorize", (req, res) => {
 
     res.end();
 });
+
+interface RelationsPostBody {
+    object: Obj;
+    name: string;
+    subject: Subject;
+}
+
+app.post(
+    "/relations",
+    (req: Request<unknown, unknown, RelationsPostBody>, res) => {
+        const obj = new Obj(req.body.object.type, req.body.object.identifier);
+        let subject: Subject;
+        // subject is UserId
+        if (typeof req.body.subject === "number") {
+            subject = req.body.subject;
+        } else {
+            const bodyObj = req.body.subject.object;
+            const object = new Obj(bodyObj.type, bodyObj.identifier);
+
+            subject = new UserSet(object, req.body.subject.relationName);
+        }
+
+        try {
+            graph.addEdge(obj, req.body.name, subject);
+        } catch (err) {
+            if (!(err instanceof Error)) {
+                console.error("Error is unknown type: ", err);
+                res.status(500).send({ error: "Whoopsies" }).end();
+                return;
+            }
+
+            res.status(409)
+                .send({
+                    error: `[${err.name}]: ${err.message}`,
+                })
+                .end();
+
+            return;
+        }
+
+        res.status(200).end();
+    }
+);
+
+// Remove relation from graph
+// app.delete("/relations", (req, res) => {
+//
+// })
+
+// Modify existing relation
+// app.put("/relations", (req, res) => {
+//
+// })
+
+// Add new object to graph
+// app.post("/objects", (req, res) => {
+//
+// })
+
+// Remove object from graph
+// app.delete("/objects", (req, res) => {
+//
+// })
+
+// Modify existing object
+// app.put("/objects", (req, res) => {
+//
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
