706cd70f6eaafc3d93566da87963016e5481705b
vicco1337 <hustedacount@gmail.com>
2026-05-07 11:00:32 +0200

    refactor(acl): port post relations to zod

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { z } from "zod";
-import { Obj, Relation, UserSet } from "./acl.ts";
+import { Obj, Relation, UserSet, type Subject } from "./acl.ts";
 import { deserializeConfig } from "./serialize.ts";
 
 const app = express();
@@ -9,10 +9,10 @@ const graph = await deserializeConfig();
 app.use(express.json()); // turns out body-parser isnt needed, express has its own json middleware
 
 const AuthorizeQuerySchema = z.object({
-    ObjectId: z.string().min(1), // .min(1) ensures no empty strings. without it, /authorize?ObjectId=&... would be valid input
-    RelationName: z.string().min(1),
-    Type: z.string().min(1),
-    UserId: z.coerce.number().min(0), // we coerce because the input will be something like "1" and we want 1
+    objectId: z.string().min(1), // .min(1) ensures no empty strings. without it, /authorize?ObjectId=&... would be valid input
+    relationName: z.string().min(1),
+    type: z.string().min(1),
+    userId: z.coerce.number().min(0), // we coerce because the input will be something like "1" and we want 1
 });
 
 app.get("/authorize", (req, res) => {
@@ -29,18 +29,18 @@ app.get("/authorize", (req, res) => {
     }
 
     // at this point we know that the request fits the schema, so the types are ensured
-    const { Type, ObjectId, RelationName, UserId } = result.data;
+    const { type, objectId, relationName, userId } = result.data;
 
     const relation = new Relation(
-        new Obj(Type, ObjectId),
-        RelationName,
-        UserId
+        new Obj(type, objectId),
+        relationName,
+        userId
     ); // merely constructing this to include its zanzibar-style string form in the response
 
-    const object = new Obj(Type, ObjectId);
-    const users = graph.resolveSubjects(new UserSet(object, RelationName));
+    const object = new Obj(type, objectId);
+    const users = graph.resolveSubjects(new UserSet(object, relationName));
 
-    if (users.has(UserId)) {
+    if (users.has(userId)) {
         res.status(200).send(
             `Relation <code>${relation.toString()}</code> exists; permission granted`
         );
@@ -51,48 +51,72 @@ app.get("/authorize", (req, res) => {
     }
 });
 
-interface RelationsPostBody {
-    object: Obj;
-    name: string;
-    subject: Subject;
-}
+const ObjectSchema = z.object({
+    type: z.string().min(1), // .min(1) ensures no empty strings. without it, /authorize?ObjectId=&... would be valid input
+    identifier: z.string().min(1),
+});
 
-app.post(
-    "/relations",
-    (req: Request<unknown, unknown, RelationsPostBody>, res) => {
-        const obj = new Obj(req.body.object.type, req.body.object.identifier);
-        let subject: Subject;
-        // subject is UserId
-        if (typeof req.body.subject === "number") {
-            subject = req.body.subject;
-        } else {
-            const bodyObj = req.body.subject.object;
-            const object = new Obj(bodyObj.type, bodyObj.identifier);
+const UserSetSchema = z.object({
+    object: ObjectSchema,
+    relationName: z.string().min(1),
+});
 
-            subject = new UserSet(object, req.body.subject.relationName);
-        }
+const SubjectSchema = z.union([UserSetSchema, z.number()]);
 
-        try {
-            graph.addEdge(obj, req.body.name, subject);
-        } catch (err) {
-            if (!(err instanceof Error)) {
-                console.error("Error is unknown type: ", err);
-                res.status(500).send({ error: "Whoopsies" }).end();
-                return;
-            }
+const RelationsQuerySchema = z.object({
+    object: ObjectSchema, // .min(1) ensures no empty strings. without it, /authorize?ObjectId=&... would be valid input
+    name: z.string().min(1),
+    subject: SubjectSchema,
+});
 
-            res.status(409)
-                .send({
-                    error: `[${err.name}]: ${err.message}`,
-                })
-                .end();
+app.post("/relations", (req, res) => {
+    const result = RelationsQuerySchema.safeParse(req.body);
 
+    if (!result.success) {
+        res.status(400)
+            .contentType("application/json")
+            .json({
+                error: "Invalid post body",
+                details: z.treeifyError(result.error),
+            });
+        return;
+    }
+
+    const { object, name } = result.data;
+
+    const obj = new Obj(object.type, object.identifier);
+
+    let subject: Subject;
+    // subject is UserId
+    if (typeof result.data.subject === "number") {
+        subject = result.data.subject;
+    } else {
+        const bodyObj = result.data.subject.object;
+        const object = new Obj(bodyObj.type, bodyObj.identifier);
+
+        subject = new UserSet(object, result.data.subject.relationName);
+    }
+
+    try {
+        graph.addEdge(obj, name, subject);
+    } catch (err) {
+        if (!(err instanceof Error)) {
+            console.error("Error is unknown type: ", err);
+            res.status(500).send({ error: "Whoopsies" }).end();
             return;
         }
 
-        res.status(200).end();
+        res.status(409)
+            .send({
+                error: `[${err.name}]: ${err.message}`,
+            })
+            .end();
+
+        return;
     }
-);
+
+    res.status(200).end();
+});
 
 // Remove relation from graph
 // app.delete("/relations", (req, res) => {
