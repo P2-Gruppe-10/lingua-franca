1599ac6567b0e6d3b2bbbd7bce5a7d29b1130d0d
victordueandersen <vdan25@student.aau.dk>
2026-05-07 14:34:07 +0200

    feat(serialize): Add hourly backup

--- src/serialize.ts
+++ src/serialize.ts
@@ -1,11 +1,30 @@
 import { promises as fs } from "node:fs";
 import Graph from "./graph.ts";
+import moment from "moment";
 
 export async function serializeConfig(graph: Graph): Promise<void> {
-    //Converts graph to JSON string and assigns it to strinifiedGraph
+    //Converts graph to JSON string and assigns it to stringifiedGraph
     const stringifiedGraph = graph.stringify();
 
-    //Overwrites config.json to stringifiedGraph
+    //Gets current time and time of last backup
+    const now = moment().format("YYYY-MM-DDTHH:mm:ss");
+    const lastBackup = (await fs.readFile("./lastBackupTime.txt"))
+        .toString()
+        .trim();
+
+    //Compare if at least one hour since last backup - if so create new backup
+    if (moment(now).isAfter(lastBackup, "hour")) {
+        //Create filepath string for new backup
+        const backupFileName = "./backup/" + now + ".json";
+
+        //Create new backup file with current graph with name from backupFileName
+        await fs.writeFile(backupFileName, stringifiedGraph);
+
+        //Update time of last backup to now
+        await fs.writeFile("./lastBackupTime.txt", now);
+    }
+
+    //Update config file with newest version of graph
     await fs.writeFile("./config.json", stringifiedGraph);
 }
 
